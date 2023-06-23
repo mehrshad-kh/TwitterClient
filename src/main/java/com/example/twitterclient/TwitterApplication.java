4d6b84cf83c03f36ebb930f2aceef7fd55a8ae98
mehrshad-kh <mjii@protonmail.com>
2023-06-23 14:40:10 +0330

    refactor: improve TwitterApplication.start()

--- src/main/java/com/example/twitterclient/TwitterApplication.java
+++ src/main/java/com/example/twitterclient/TwitterApplication.java
@@ -2,17 +2,20 @@ package com.example.twitterclient;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
-public class HelloApplication extends Application {
+public class TwitterApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader loader = new FXMLLoader(Objects.requireNonNull(getClass().getResource("hello-view.fxml")));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setTitle("Twitter");
         stage.setScene(scene);
         stage.show();
     }
