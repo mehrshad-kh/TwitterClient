0a60a9e0c0212e06342aef1200bbf5aa66077d19
mehrshad-kh <mjii@protonmail.com>
2023-06-23 14:43:07 +0330

    refactor: various renamings

--- src/main/java/com/example/twitterclient/SignInController.java
+++ src/main/java/com/example/twitterclient/SignInController.java
@@ -3,7 +3,7 @@ package com.example.twitterclient;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class SignInController {
     @FXML
     private Label welcomeText;
 
--- src/main/java/com/example/twitterclient/TwitterApplication.java
+++ src/main/java/com/example/twitterclient/TwitterApplication.java
@@ -12,7 +12,7 @@ import java.util.Objects;
 public class TwitterApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader loader = new FXMLLoader(Objects.requireNonNull(getClass().getResource("hello-view.fxml")));
+        FXMLLoader loader = new FXMLLoader(Objects.requireNonNull(getClass().getResource("sign-in-view.fxml")));
         Parent root = loader.load();
         Scene scene = new Scene(root);
         stage.setTitle("Twitter");
