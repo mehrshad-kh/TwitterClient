bdd42910e16b2f0ed7b7015199a97e13109c1069
mehrshad-kh <mjii@protonmail.com>
2023-06-24 10:41:32 +0330

    feat: add twitter.proto

--- src/main/java/com/example/twitterclient/SignUpController.java
+++ src/main/java/com/example/twitterclient/SignUpController.java
@@ -1,4 +1,18 @@
 package com.example.twitterclient;
 
-public class SignUpController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SignUpController implements Initializable {
+    @FXML
+    private ComboBox<String> countriesComboBox;
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Retrieve countries from the server.
+        // Populate the combo box.
+    }
 }
