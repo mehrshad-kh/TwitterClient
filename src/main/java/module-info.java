5cee6e2fab3133e20da3c6abe5bc14bcfc6b22c3
mehrshad-kh <mjii@protonmail.com>
2023-06-24 11:27:10 +0330

    refactor: modify module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,12 @@
 module com.mkh.twitterclient {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires protobuf.java;
+    requires grpc.api;
+    requires java.annotation;
+    requires grpc.stub;
+    requires grpc.protobuf;
+    requires com.google.common;
 
     opens com.mkh.twitterclient to javafx.fxml;
     exports com.mkh.twitterclient;
