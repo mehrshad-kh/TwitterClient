ad3b55718ebd3f65796899c9e97f03aba5e5b090
mehrshad-kh <mjii@protonmail.com>
2023-06-24 12:27:52 +0330

    feat: add TwitterClient.java

---
+++ src/main/java/com/mkh/TwitterClient.java
@@ -0,0 +1,56 @@
+package com.mkh;
+
+import com.mkh.twitter.Country;
+import com.mkh.twitter.MKEmpty;
+import com.mkh.twitter.TwitterGrpc;
+import com.mkh.twitter.TwitterGrpc.TwitterBlockingStub;
+import com.mkh.twitter.TwitterGrpc.TwitterStub;
+import io.grpc.Channel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class TwitterClient {
+    private static final Logger logger = Logger.getLogger(TwitterClient.class.getName());
+
+    private final TwitterBlockingStub blockingStub;
+    private final TwitterStub asyncStub;
+
+    public TwitterClient(String host, int port) {
+        // this(Grpc.newChannelBuilder(host + port, InsecureChannelCredentials.create()).build());
+        this(ManagedChannelBuilder.forAddress(host, port).usePlaintext());
+    }
+
+    public TwitterClient(Channel channel) {
+        blockingStub = TwitterGrpc.newBlockingStub(channel);
+        asyncStub = TwitterGrpc.newStub(channel);
+    }
+
+    public TwitterClient(ManagedChannelBuilder<?> channelBuilder) {
+        this(channelBuilder.build());
+    }
+
+    public ArrayList<Country> retrieveCountries() {
+        logger.info("retrieveCountries");
+
+        Iterator<Country> countryIterator = null;
+        try {
+            countryIterator = blockingStub.retrieveCountries(MKEmpty.newBuilder().build());
+        } catch (StatusRuntimeException e) {
+            logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
+        }
+
+        ArrayList<Country> countries = new ArrayList<>();
+        try {
+            countryIterator.forEachRemaining(countries::add);
+        } catch (NullPointerException e) {
+            return new ArrayList<>();
+        }
+
+        return countries;
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,6 +7,7 @@ module com.mkh.twitterclient {
     requires grpc.stub;
     requires grpc.protobuf;
     requires com.google.common;
+    requires java.logging;
 
     opens com.mkh.twitterclient to javafx.fxml;
     exports com.mkh.twitterclient;
